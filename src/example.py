da5fbb1d78771ae7a89cedba10782fc9f395fab4
kwintenvm <kwinten.vanmeerbeek@gmail.com>
2021-04-13 15:11:24 +0200

    example oops

---
+++ src/example.py
@@ -0,0 +1,9 @@
+from PyQt5 import QtWidgets
+from databasemanager import *
+from database_explorer_plotter.database_ui import database_ui
+import sys
+
+app = QtWidgets.QApplication(sys.argv)
+w = database_ui()
+w.show()
+sys.exit(app.exec_()) 
