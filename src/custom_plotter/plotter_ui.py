5d42c559bcc9d42839b46c385aa2f2367c9f1b4b
jowanpittevils <jowan.pittevils@student.kuleuven.be>
2021-04-26 14:38:18 +0200

    necessary

--- src/custom_plotter/plotter_ui.py
+++ src/custom_plotter/plotter_ui.py
@@ -272,49 +272,49 @@ class plotter_ui(QObject, Ui_MainWindow):
 
     
     def __AssignCallbacks(self):
-        # move buttons
-        self.btnFirst.clicked.connect(self.__onbtnFirstClicked)
-        self.btnLast.clicked.connect(self.__onbtnLastClicked)
-        self.btnPrevious.clicked.connect(self.__onbtnPreviousClicked)
-        self.btnNext.clicked.connect(self.__onbtnNextClicked)
-        self.btnPreviousSimilarY.clicked.connect(self.__onbtnPreviousSimilarYClicked)
-        self.btnNextSimilarY.clicked.connect(self.__onbtnNextSimilarYClicked)
-        #slider+spinbox
-        self.sldSampleIndex.valueChanged.connect(self.__onsldValueChanged)
-        self.nmrSampleIndex.valueChanged.connect(self.__onnmrValueChanged)
-        #amplitude+timescale buttons
-        self.btnAmpUp.clicked.connect(self.__increase_amplitude)
-        self.btnAmpDown.clicked.connect(self.__decrease_amplitude)
-        self.btnWindowUp.clicked.connect(self.__onbtnWindowUp)
-        self.btnWindowDown.clicked.connect(self.__onbtnWindowDown)
-        #checkboxes + duplicate&print buttons
-        self.chbNight.stateChanged.connect(self.__onchbNightStateChanged)        
-        self.chbFavorite.stateChanged.connect(self.__onchbFavoriteStateChanged)
-        self.chbFit.stateChanged.connect(self.__onchbFitStateChanged)
-        self.btnPrint.clicked.connect(self.__onbtnPrintClicked)
-        self.btnDuplicate.clicked.connect(self.__onbtnDuplicate)
-        ###menubar###
-        #channels
-        self.signals_add.triggered.connect(self.__onbtnChannelsAdd)
-        self.signals_remove.triggered.connect(self.__onbtnChannelsRemove)
-        #timescale
-        self.window1sec.triggered.connect(lambda: self.__windowResize(1))
-        self.window2sec.triggered.connect(lambda: self.__windowResize(2))
-        self.window5sec.triggered.connect(lambda: self.__windowResize(5))
-        self.window10sec.triggered.connect(lambda: self.__windowResize(10))
-        self.window20sec.triggered.connect(lambda: self.__windowResize(20))
-        self.window30sec.triggered.connect(lambda: self.__windowResize(30))
-        self.window60sec.triggered.connect(lambda: self.__windowResize(60))
-        #amplitude
-        self.amp5x.triggered.connect(lambda: self.__amplitudeResize(5))
-        self.amp2x.triggered.connect(lambda: self.__amplitudeResize(2))
-        self.amp1_5x.triggered.connect(lambda: self.__amplitudeResize(1.5))
-        self.amp1_2x.triggered.connect(lambda: self.__amplitudeResize(1.2))
-        self.amp1x.triggered.connect(lambda: self.__amplitudeResize(1))
-        self.amp0_8x.triggered.connect(lambda: self.__amplitudeResize(0.8))
-        self.amp0_5x.triggered.connect(lambda: self.__amplitudeResize(0.5))
-        self.amp0_3x.triggered.connect(lambda: self.__amplitudeResize(0.3))
-        self.amp0_1x.triggered.connect(lambda: self.__amplitudeResize(0.1))
+    # move buttons
+    self.btnFirst.clicked.connect(self.__onbtnFirstClicked)
+    self.btnLast.clicked.connect(self.__onbtnLastClicked)
+    self.btnPrevious.clicked.connect(self.__onbtnPreviousClicked)
+    self.btnNext.clicked.connect(self.__onbtnNextClicked)
+    self.btnPreviousSimilarY.clicked.connect(self.__onbtnPreviousSimilarYClicked)
+    self.btnNextSimilarY.clicked.connect(self.__onbtnNextSimilarYClicked)
+    #slider+spinbox
+    self.sldSampleIndex.valueChanged.connect(self.__onsldValueChanged)
+    self.nmrSampleIndex.valueChanged.connect(self.__onnmrValueChanged)
+    #amplitude+timescale buttons
+    self.btnAmpUp.clicked.connect(self.__increase_amplitude)
+    self.btnAmpDown.clicked.connect(self.__decrease_amplitude)
+    self.btnWindowUp.clicked.connect(self.__onbtnWindowUp)
+    self.btnWindowDown.clicked.connect(self.__onbtnWindowDown)
+    #checkboxes + duplicate&print buttons
+    self.chbNight.stateChanged.connect(self.__onchbNightStateChanged)        
+    self.chbFavorite.stateChanged.connect(self.__onchbFavoriteStateChanged)
+    self.chbFit.stateChanged.connect(self.__onchbFitStateChanged)
+    self.btnPrint.clicked.connect(self.__onbtnPrintClicked)
+    self.btnDuplicate.clicked.connect(self.__onbtnDuplicate)
+    ###menubar###
+    #channels
+    self.signals_add.triggered.connect(self.__onbtnChannelsAdd)
+    self.signals_remove.triggered.connect(self.__onbtnChannelsRemove)
+    #timescale
+    self.window1sec.triggered.connect(lambda: self.__windowResize(1))
+    self.window2sec.triggered.connect(lambda: self.__windowResize(2))
+    self.window5sec.triggered.connect(lambda: self.__windowResize(5))
+    self.window10sec.triggered.connect(lambda: self.__windowResize(10))
+    self.window20sec.triggered.connect(lambda: self.__windowResize(20))
+    self.window30sec.triggered.connect(lambda: self.__windowResize(30))
+    self.window60sec.triggered.connect(lambda: self.__windowResize(60))
+    #amplitude
+    self.amp5x.triggered.connect(lambda: self.__amplitudeResize(5))
+    self.amp2x.triggered.connect(lambda: self.__amplitudeResize(2))
+    self.amp1_5x.triggered.connect(lambda: self.__amplitudeResize(1.5))
+    self.amp1_2x.triggered.connect(lambda: self.__amplitudeResize(1.2))
+    self.amp1x.triggered.connect(lambda: self.__amplitudeResize(1))
+    self.amp0_8x.triggered.connect(lambda: self.__amplitudeResize(0.8))
+    self.amp0_5x.triggered.connect(lambda: self.__amplitudeResize(0.5))
+    self.amp0_3x.triggered.connect(lambda: self.__amplitudeResize(0.3))
+    self.amp0_1x.triggered.connect(lambda: self.__amplitudeResize(0.1))
 
     #move buttons
     def __onbtnFirstClicked(self):
