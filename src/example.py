0677e02caa5d27a11edf61527af77c6d3b7f7cbf
kwintenvm <kwinten.vanmeerbeek@gmail.com>
2021-05-04 23:28:22 +0200

    fixed a bug with config and separate datasets folder

--- src/example.py
+++ src/example.py
@@ -3,7 +3,9 @@ from databasemanager import *
 from database_explorer_plotter.database_ui import database_ui
 import sys
 
-
+QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+if hasattr(QtWidgets.QStyleFactory, "AA_UseHighDpiPixmaps"):
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps)
 app = QtWidgets.QApplication(sys.argv)
 w = database_ui()
 w.show()
